cd741c6b9a52383ade859e395248f6cdfea61eb8
RaylILYn <rednoxmc@mail.ru>
2023-03-23 20:40:48 +0300

    Добавьте файлы проекта.

---
+++ OOP Math Function/OOP Math Function/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задание 1.1 [Вариант 2]
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z;
+            double m;
+
+            Console.Write("Введите x= ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите b= ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            m = (1.5 + b) - x * b;
+            if (m == 0)
+            {
+                Console.Write("Ошибка! Деление на ноль");
+                return;
+            }
+            z = Math.Pow(x, 3) / (1.5 + b) - x * b;
+            Console.WriteLine("\nx={0}\tb={1}\nf={2,6:f3}", x, b, z);
+            Console.ReadKey();
+        }
+    }
+}
